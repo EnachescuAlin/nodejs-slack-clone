d4425913a95c577102322e66603c3486f10bf8b9
Alin Enachescu <alinenachescu033@gmail.com>
2018-10-20 12:50:50 +0300

    implemented signUp and get users

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const userModel = require('./userModel');
 
 class Server
 {
     constructor()
     {
+        mongoose.connect('mongodb://localhost/slack_clone_db', (err) => {
+            if (err) {
+                console.log('connect to database failed =', err);
+            } else {
+                console.log('connected to database successfully');
+            }
+        });
+
         this.server = express(http);
         this.server.use(express.json());
 
         this.server.get('/', this.processingGet);
+        this.server.get('/users', this.handleGetUsers);
 
         this.server.post('/signUp', this.handleSignUp);
     }
@@ -26,8 +38,46 @@ class Server
 
     handleSignUp(req, res)
     {
-        console.log('handle sign up, body =', req.body);
-        res.send(req.body);
+        const username = req.body.username;
+        const password = req.body.password;
+        console.log('handle sign up, username =', username, ', password =', password);
+
+        userModel.findOne({ 'username': username }, (err, user) => {
+            if (err) {
+                res.send({ 'error': 'could not execute findOne method' });
+                return;
+            }
+            if (user) {
+                res.send({ 'error': 'user already exists' });
+                return;
+            }
+
+            const newUser = new userModel();
+            newUser.username = username;
+            newUser.password = password;
+
+            newUser.save((err, user) => {
+                if (err) {
+                    res.send({ 'error': 'could not save user' });
+                    return;
+                }
+
+                res.send({ 'error': 'ok' });
+            });
+        });
+    }
+
+    handleGetUsers(req, res)
+    {
+        console.log('handle get users');
+        userModel.find((err, users) => {
+            if (err) {
+                res.send({ 'error': 'could not get users' });
+                return;
+            }
+
+            res.send(users);
+        });
     }
 }
 
