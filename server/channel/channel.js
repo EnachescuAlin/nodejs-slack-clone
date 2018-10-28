e43260d0c799dc4bd1711ebbd20b3722042be4e1
Alin Enachescu <alinenachescu033@gmail.com>
2018-10-28 17:47:02 +0200

    implemented toDto function; changed the type for some fields

--- server/channel/channel.js
+++ server/channel/channel.js
@@ -4,7 +4,8 @@ const channelSchema = new mongoose.Schema({
     _id: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
-        auto: true
+        auto: true,
+        alias: 'id'
     },
     name: {
         type: mongoose.Schema.Types.String,
@@ -12,23 +13,38 @@ const channelSchema = new mongoose.Schema({
         required: true
     },
     description: {
-        type: mongoose.Schema.Types.String
+        type: mongoose.Schema.Types.String,
+        default: ""
     },
     isPublic: {
-        type: mongoose.Schema.Types.Boolean
+        type: mongoose.Schema.Types.Boolean,
+        required: true
     },
     creationDate: {
         type: mongoose.Schema.Types.Date,
         default: Date.now
     },
     createdBy: {
-        type: mongoose.Schema.Types.Number
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
     },
     members: [
         {
-            type: mongoose.Schema.Types.Number
+            type: mongoose.Schema.Types.ObjectId
         }
     ]
 });
+
+channelSchema.methods.toDto = function() {
+    return {
+        id: this._id,
+        name: this.name,
+        description: this.description,
+        isPublic: this.isPublic,
+        creationDate: this.creationDate,
+        createdBy: this.createdBy,
+        members: this.members
+    };
+};
 
 export default mongoose.model('Channel', channelSchema);
