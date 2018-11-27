afac301fa18526951279f191630b8f19e0da7a90
Alin Enachescu <alinenachescu033@gmail.com>
2018-11-27 20:31:30 +0200

    implemented tests for getById, getCurrent and update

--- test/userController.test.js
+++ test/userController.test.js
@@ -68,6 +68,7 @@ describe('User', () => {
                 res.body.should.have.property('channels');
                 res.body.should.have.property('online');
                 res.body.should.have.property('directMessages');
+                res.body.should.have.property('status');
 
                 res.body.username.should.be.eql(users[i].user.username);
                 res.body.firstname.should.be.eql(users[i].user.firstName);
@@ -76,6 +77,7 @@ describe('User', () => {
                 res.body.online.should.be.eql(false);
                 res.body.channels.should.be.eql([]);
                 res.body.directMessages.should.be.eql([]);
+                res.body.status.should.be.eql("");
             }
         });
 
@@ -255,6 +257,7 @@ describe('User', () => {
                 res.body.should.have.property('channels');
                 res.body.should.have.property('online');
                 res.body.should.have.property('directMessages');
+                res.body.should.have.property('status');
 
                 res.body.username.should.be.eql(users[i].user.username);
                 res.body.firstname.should.be.eql(users[i].user.firstName);
@@ -263,6 +266,7 @@ describe('User', () => {
                 res.body.online.should.be.eql(online);
                 res.body.channels.should.be.eql([]);
                 res.body.directMessages.should.be.eql([]);
+                res.body.status.should.be.eql("");
             }
 
             await chai.request(server).post('/api/users/logout')
@@ -296,6 +300,271 @@ describe('User', () => {
     
             res.body.should.have.property('error');
             res.body.error.should.be.eql('User with username qweasdzxc was not found');
+
+            await chai.request(server).post('/api/users/logout')
+                .auth(loggedUser.body.token, { type: 'bearer' }).send();
+        });
+    });
+
+    describe('getById', () => {
+        it('it should get an user by id', async () => {
+            const user = {
+                username: users[0].user.username,
+                password: users[0].user.password
+            };
+            const loggedUser = await chai.request(server).post('/api/users/login').send(user);
+            const allUsers = await chai.request(server).get('/api/users/')
+                .auth(loggedUser.body.token, { type: 'bearer' });
+
+            for (let i = 0; i < users.length; i++) {
+                let online = false;
+                if (i === 0) {
+                    online = true;
+                }
+
+                const res = await chai.request(server).get(`/api/users/${allUsers.body[i].id}`)
+                    .auth(loggedUser.body.token, { type: 'bearer' });
+
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+
+                res.body.should.not.have.property('error');
+
+                res.body.should.have.property('id');
+                res.body.should.have.property('username');
+                res.body.should.have.property('firstname');
+                res.body.should.have.property('lastname');
+                res.body.should.have.property('registerDate');
+                res.body.should.have.property('email');
+                res.body.should.have.property('channels');
+                res.body.should.have.property('online');
+                res.body.should.have.property('directMessages');
+                res.body.should.have.property('status');
+
+                res.body.username.should.be.eql(users[i].user.username);
+                res.body.firstname.should.be.eql(users[i].user.firstName);
+                res.body.lastname.should.be.eql(users[i].user.lastName);
+                res.body.email.should.be.eql(users[i].user.email);
+                res.body.online.should.be.eql(online);
+                res.body.channels.should.be.eql([]);
+                res.body.directMessages.should.be.eql([]);
+                res.body.status.should.be.eql("");
+            }
+
+            await chai.request(server).post('/api/users/logout')
+                .auth(loggedUser.body.token, { type: 'bearer' }).send();
+        });
+
+        it('it should not get an user by id without auth', async () => {
+            const user = {
+                username: users[0].user.username,
+                password: users[0].user.password
+            };
+            const loggedUser = await chai.request(server).post('/api/users/login').send(user);
+            const infoUser = await chai.request(server).get('/api/users/')
+                .auth(loggedUser.body.token, { type: 'bearer' })
+                .query({ 'username': user.username });
+
+            let res = await chai.request(server).get(`/api/users/${infoUser.body.id}`);
+
+            res.should.have.status(401);
+            res.body.should.be.a('object');
+
+            res.body.should.have.property('error');
+            res.body.error.should.be.eql('Invalid Token');
+
+            await chai.request(server).post('/api/users/logout')
+                .auth(loggedUser.body.token, { type: 'bearer' }).send();
+        });
+
+        it('it should not get a non-existent user by id', async () => {
+            const user = {
+                username: users[0].user.username,
+                password: users[0].user.password
+            };
+            const loggedUser = await chai.request(server).post('/api/users/login').send(user);
+
+            const res = await chai.request(server).get('/api/users/123')
+                .auth(loggedUser.body.token, { type: 'bearer' });
+
+            res.should.have.status(404);
+            res.body.should.be.a('object');
+
+            res.body.should.have.property('error');
+            res.body.error.should.be.eql('User with id 123 was not found');
+
+            await chai.request(server).post('/api/users/logout')
+                .auth(loggedUser.body.token, { type: 'bearer' }).send();
+        });
+    });
+
+    describe('getCurrent', () => {
+        it('it should get current logged user', async () => {
+            const user = {
+                username: users[0].user.username,
+                password: users[0].user.password
+            };
+            const loggedUser = await chai.request(server).post('/api/users/login').send(user);
+
+            const res = await chai.request(server).get('/api/users/authenticated/current')
+                .auth(loggedUser.body.token, { type: 'bearer' });
+
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+
+            res.body.should.not.have.property('error');
+
+            res.body.should.have.property('id');
+            res.body.should.have.property('username');
+            res.body.should.have.property('firstname');
+            res.body.should.have.property('lastname');
+            res.body.should.have.property('registerDate');
+            res.body.should.have.property('email');
+            res.body.should.have.property('channels');
+            res.body.should.have.property('online');
+            res.body.should.have.property('directMessages');
+            res.body.should.have.property('status');
+
+            res.body.username.should.be.eql(users[0].user.username);
+            res.body.firstname.should.be.eql(users[0].user.firstName);
+            res.body.lastname.should.be.eql(users[0].user.lastName);
+            res.body.email.should.be.eql(users[0].user.email);
+            res.body.online.should.be.eql(true);
+            res.body.channels.should.be.eql([]);
+            res.body.directMessages.should.be.eql([]);
+            res.body.status.should.be.eql("");
+
+            await chai.request(server).post('/api/users/logout')
+                .auth(loggedUser.body.token, { type: 'bearer' }).send();
+        });
+
+        it('it should not get an user without auth', async () => {
+            let res = await chai.request(server).get('/api/users/authenticated/current');
+
+            res.should.have.status(401);
+            res.body.should.be.a('object');
+
+            res.body.should.have.property('error');
+            res.body.error.should.be.eql('Invalid Token');
+        });
+    });
+
+    describe('update', () => {
+        it('it should update an user', async () => {
+            const user = {
+                username: users[0].user.username,
+                password: users[0].user.password
+            };
+            const newUser = {
+                username: "admin4",
+                password: "admin4",
+                email: "admin4@gmail.com",
+                firstName: "admin4",
+                lastName: "admin4",
+                status: "admin4"
+            };
+            let loggedUser = await chai.request(server).post('/api/users/login').send(user);
+            let infoUser = await chai.request(server).get('/api/users/')
+                .auth(loggedUser.body.token, { type: 'bearer' })
+                .query({ 'username': user.username });
+
+            let res = await chai.request(server).put(`/api/users/${infoUser.body.id}`)
+                .auth(loggedUser.body.token, { type: 'bearer' }).send(newUser);
+
+            res.should.have.status(204);
+
+            await chai.request(server).post('/api/users/logout')
+                .auth(loggedUser.body.token, { type: 'bearer' }).send();
+
+            loggedUser = await chai.request(server).post('/api/users/login')
+                .send({ username: newUser.username, password: newUser.password });
+            res = await chai.request(server).get('/api/users/')
+                .auth(loggedUser.body.token, { type: 'bearer' })
+                .query({ 'username': newUser.username });
+
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+
+            res.body.should.not.have.property('error');
+
+            res.body.should.have.property('id');
+            res.body.should.have.property('username');
+            res.body.should.have.property('firstname');
+            res.body.should.have.property('lastname');
+            res.body.should.have.property('registerDate');
+            res.body.should.have.property('email');
+            res.body.should.have.property('channels');
+            res.body.should.have.property('online');
+            res.body.should.have.property('directMessages');
+            res.body.should.have.property('status');
+
+            res.body.username.should.be.eql(newUser.username);
+            res.body.firstname.should.be.eql(newUser.firstName);
+            res.body.lastname.should.be.eql(newUser.lastName);
+            res.body.email.should.be.eql(newUser.email);
+            res.body.online.should.be.eql(true);
+            res.body.channels.should.be.eql([]);
+            res.body.directMessages.should.be.eql([]);
+            res.body.status.should.be.eql(newUser.status);
+
+            await chai.request(server).post('/api/users/logout')
+                .auth(loggedUser.body.token, { type: 'bearer' }).send();
+
+            users[0].user = {
+                username: "admin4",
+                password: "admin4",
+                email: "admin4@gmail.com",
+                firstName: "admin4",
+                lastName: "admin4"
+            };
+        });
+
+        it('it should not update an user with an existing username', async () => {
+            const user = {
+                username: users[0].user.username,
+                password: users[0].user.password
+            };
+            const loggedUser = await chai.request(server).post('/api/users/login').send(user);
+            const infoUser = await chai.request(server).get('/api/users/')
+                .auth(loggedUser.body.token, { type: 'bearer' })
+                .query({ 'username': user.username });
+
+            let res = await chai.request(server).put(`/api/users/${infoUser.body.id}`)
+                .auth(loggedUser.body.token, { type: 'bearer' })
+                .send({ 'username': 'admin2' });
+
+            res.should.have.status(422);
+            res.body.should.be.a('object');
+
+            res.body.should.have.property('error');
+            res.body.error.should.be.eql('Username "admin2" is already taken');
+
+            await chai.request(server).post('/api/users/logout')
+                .auth(loggedUser.body.token, { type: 'bearer' }).send();
+        });
+
+        it('it should not update an user without auth', async () => {
+            let res = await chai.request(server).put('/api/users/123');
+
+            res.should.have.status(401);
+            res.body.should.be.a('object');
+
+            res.body.should.have.property('error');
+            res.body.error.should.be.eql('Invalid Token');
+        });
+
+        it('it should not update other user', async () => {
+            const user = {
+                username: users[0].user.username,
+                password: users[0].user.password
+            };
+            const loggedUser = await chai.request(server).post('/api/users/login').send(user);
+
+            let res = await chai.request(server).put('/api/users/123')
+                .auth(loggedUser.body.token, { type: 'bearer' });
+
+            res.should.have.status(401);
+            res.body.should.be.a('object');
 
             await chai.request(server).post('/api/users/logout')
                 .auth(loggedUser.body.token, { type: 'bearer' }).send();
